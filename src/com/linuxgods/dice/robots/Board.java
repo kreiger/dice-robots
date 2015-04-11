f38157a5c62f067f5047ee9a7a8094fb16c62584
Christoffer Hammarström <kreiger@linuxgods.com>
2015-04-11 11:34:48 +0200

    Some initial board logic to find player and tile contents in map.

--- src/com/linuxgods/dice/robots/Board.java
+++ src/com/linuxgods/dice/robots/Board.java
@@ -1,9 +1,37 @@
 package com.linuxgods.dice.robots;
 
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Board {
     public static final Dimension TILES = new Dimension(100, 100);
+    private Map<Coordinate, TileContent> tiles = new HashMap<Coordinate, TileContent>();
+
+    private Optional<TileContent> getTile(Coordinate coordinate) {
+        return Optional.ofNullable(tiles.get(coordinate));
+    }
+
+    public Coordinate getPlayerCoordinate() {
+        return getCoordinatesFor(TileContent.PLAYER)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("No player!?"));
+    }
+
+    public Stream<Coordinate> getCoordinatesFor(TileContent tileContent) {
+        return tiles.entrySet().stream()
+                .filter(entry -> entry.getValue() == tileContent)
+                .map(Map.Entry::getKey);
+    }
+
+    public enum TileContent {
+        PLAYER,
+        ROBOT,
+        SCRAP;
+    }
 
     public static class Coordinate {
         public final int x,y;
@@ -11,6 +39,20 @@ public class Board {
         public Coordinate(int x, int y) {
             this.x = x;
             this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Coordinate that = (Coordinate) o;
+            return Objects.equals(x, that.x) &&
+                    Objects.equals(y, that.y);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
         }
     }
 
