43dc00cb5764383f25aa20a9085c9853c33ecc71
Christoffer Hammarström <kreiger@linuxgods.com>
2015-04-11 13:23:30 +0200

    Exit on window close.

--- src/com/linuxgods/dice/robots/MainFrame.java
+++ src/com/linuxgods/dice/robots/MainFrame.java
@@ -7,7 +7,7 @@ public class MainFrame extends JFrame {
     public MainFrame(BoardGraphicsComponent boardGraphicsComponent) {
         getContentPane().add(boardGraphicsComponent);
         pack();
-
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
     }
 }
